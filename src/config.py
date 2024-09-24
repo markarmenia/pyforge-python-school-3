7c044b46ba6d2aa0b2f46274fd38ed945015b851
nshan_yegyan23 <nshan_yegyan23@alumni.aua.am>
2024-09-24 20:24:42 +0400

    added new deploy.yml

--- src/config.py
+++ src/config.py
@@ -1,6 +1,10 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
